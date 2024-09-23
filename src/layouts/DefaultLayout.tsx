bea25bdeb9aaa4668739bdfef0e78c37447a4ea6
nara <parknara0213@gmail.com>
2024-09-24 01:33:04 +0900

    Feat: 추가 접속 국가 알아내고 저장하기
    Refs: #8

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,7 +1,24 @@
 import { ColorModeButton } from "@/components";
+import { changeLanguage } from "@/store/slices/settingsSlice";
+import { RootState } from "@/store/store";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 const DefaultLayout = () => {
+  const dispatch = useDispatch();
+  const language = useSelector((state: RootState) => state.settings.language);
+
+  // 사이트에 접속한 유저의 언어 설정 알아내기
+  useEffect(() => {
+    // 선호 언어에서 첫 두문자만 추출
+    const lang = navigator.language.slice(0, 2);
+
+    if (language === lang) return;
+
+    dispatch(changeLanguage(lang));
+  }, []);
+
   return (
     <>
       <ColorModeButton />
--- src/store/slices/settingsSlice.ts
+++ src/store/slices/settingsSlice.ts
@@ -2,10 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface SettingsState {
   colorTheme: string;
+  language: string;
 }
 
 const initialState: SettingsState = {
   colorTheme: "light",
+  language: "ko",
 };
 
 const settingsSlice = createSlice({
@@ -15,8 +17,12 @@ const settingsSlice = createSlice({
     changeColorTheme: (state, action) => {
       state.colorTheme = action.payload;
     },
+    changeLanguage: (state, action) => {
+      if (state.language === action.payload) return;
+      state.language = action.payload;
+    },
   },
 });
 
 export default settingsSlice.reducer;
-export const { changeColorTheme } = settingsSlice.actions;
+export const { changeColorTheme, changeLanguage } = settingsSlice.actions;
