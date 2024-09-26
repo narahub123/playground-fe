ec5504c8fbc85228e7fee985d32d48b2022112eb
nara <parknara0213@gmail.com>
2024-09-26 17:27:44 +0900

    Feat: focus를 이용한 부드러운 전환 효과 적용하기
    - CSS 전환 효과 사용
    
    Refs: #15 Related to: #2

--- src/features/authentication/components/AuthInput/AuthInput.tsx
+++ src/features/authentication/components/AuthInput/AuthInput.tsx
@@ -1,16 +1,34 @@
 import { LuChevronDown } from "react-icons/lu";
 import styles from "./AuthInput.module.css";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 interface AuthInputProps {
   title: string;
-  limit?: number; // 최대 입력 가능 글자 수
-  list?: [];
+  limit?: number; // 최대 입력 가능 글 자 수
+  list?: []; // 목록
+  extra?: ReactNode; // 추가 기능 삽입
 }
 
-const AuthInput = ({ title, limit, list }: AuthInputProps) => {
+const AuthInput = ({ title, limit, list, extra }: AuthInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [focused, setFocused] = useState(false);
+  const focusCond = focused ? styles.focused : "";
+
+  useEffect(() => {
+    if (!inputRef.current) return;
+    if (focused) {
+      inputRef.current.focus();
+    }
+  }, [focused]);
+
   return (
     // AuthInput의 틀: 높이는 고정되어 있고 width는 가변적임
-    <div className={styles.wrapper}>
+    <div
+      className={`${styles.wrapper} ${focusCond}`}
+      onClick={() => setFocused(true)}
+      onFocus={() => setFocused(true)}
+      tabIndex={0}
+    >
       {/* 목록이 있는 경우 메인없는 경우에는 wrapper가 하나 더 있는 것임 */}
       {/* 목록이 없는 경우를 상정해서 flex: 1 설정 */}
       <span className={styles.main}>
@@ -18,15 +36,28 @@ const AuthInput = ({ title, limit, list }: AuthInputProps) => {
         <div className={styles.info}>
           {/* 좌측 */}
           <span className={styles.left}>
-            <p className={styles.title}>{title}</p>
+            <p className={`${styles.title} ${focusCond}`}>{title}</p>
           </span>
           {/* 우측 */}
           <span className={styles.right}>
-            {limit && <p className={styles.count}>0 / {limit}</p>}
+            {limit && focused && (
+              <p className={styles.count}>
+                <span className={styles.length}>0</span> / {limit}
+              </p>
+            )}
           </span>
         </div>
         {/* input 필드: 목록이 있는 것을 다루면 코드 추가 예정*/}
-        <input type="text" className={styles.input} />
+        <div className={`${styles.box} ${focusCond}`}>
+          <input
+            type="text"
+            className={`${styles.input} ${focusCond}`}
+            ref={inputRef}
+            onBlur={() => setFocused(false)}
+          />
+          {/* 간단한 버튼 등을 삽입 가능 */}
+          {extra}
+        </div>
       </span>
       {list && (
         <span className={styles.icon}>
--- src/features/authentication/signup/EmailSignup/UserInfo/UserInfo.tsx
+++ src/features/authentication/signup/EmailSignup/UserInfo/UserInfo.tsx
@@ -1,6 +1,7 @@
 import { CONSTANT } from "@/constants";
 import styles from "./UserInfo.module.css";
 import { AuthInput } from "@/features/authentication/components";
+import { LuEye } from "react-icons/lu";
 
 const UserInfo = () => {
   return (
@@ -9,23 +10,23 @@ const UserInfo = () => {
         <AuthInput title="이름" limit={CONSTANT.MAX_USERNAME} />
       </section>
       <section className={styles.field}>
-        <AuthInput title="성별" limit={CONSTANT.MAX_USERNAME} />
+        <AuthInput title="성별" />
       </section>
       <section className={styles.field}>
-        <AuthInput title="이메일" limit={CONSTANT.MAX_USERNAME} />
+        <AuthInput title="이메일" extra={<LuEye className="icon" />} />
       </section>
       <section className={styles.container}>
         <p className={styles.title}>생년월일</p>
         <p className={styles.detail}>어쩌구 저쩌구</p>
         <div className={styles.birth}>
           <span className={styles.field}>
-            <AuthInput title="년" limit={CONSTANT.MAX_USERNAME} />
+            <AuthInput title="년" />
           </span>
           <span className={styles.field}>
-            <AuthInput title="월" limit={CONSTANT.MAX_USERNAME} />
+            <AuthInput title="월" />
           </span>
           <span className={styles.field}>
-            <AuthInput title="일" limit={CONSTANT.MAX_USERNAME} />
+            <AuthInput title="일" />
           </span>
         </div>
       </section>
